c08023ce2b718f4fa26a1c00d23f9b37af6b7999
snishimura <sjiro.nishimura@gmail.com>
2018-06-08 19:57:29 +0900

    Initial Commit

---
+++ OfflineNavi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfflineNavi
+//
+//  Created by snishimura on 2018/06/06.
+//  Copyright © 2018年 snishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
